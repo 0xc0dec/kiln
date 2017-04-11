e076d1fc9a683fe6d603d108f7076728c3a802c3
Aleksey Fedotov <alexey.fedotov@fastdev.se>
2017-04-11 11:54:11 +0500

    Test code using SDL

--- src/Demo1/Demo1.cpp
+++ src/Demo1/Demo1.cpp
@@ -1,4 +1,28 @@
+#include <SDL.h>
+#include <SDL_surface.h>
+
 int main()
 {
+    SDL_GL_SetAttribute(SDL_GL_CONTEXT_PROFILE_MASK, SDL_GL_CONTEXT_PROFILE_CORE);
+    SDL_GL_SetAttribute(SDL_GL_RED_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_GREEN_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_BLUE_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_ALPHA_SIZE, 8);
+    SDL_GL_SetAttribute(SDL_GL_DEPTH_SIZE, 16);
+    SDL_GL_SetAttribute(SDL_GL_DOUBLEBUFFER, 1);
+
+    uint32_t flags = SDL_WINDOW_OPENGL | SDL_WINDOW_ALLOW_HIGHDPI;
+
+    auto window = SDL_CreateWindow("Test", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, flags);
+    auto context = SDL_GL_CreateContext(window);
+
+    SDL_GL_SetSwapInterval(1);
+
+    // Shutdown
+    if (context)
+        SDL_GL_DeleteContext(context);
+    if (window)
+        SDL_DestroyWindow(window);
+
     return 0;
 }
