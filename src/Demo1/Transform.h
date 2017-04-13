43257561ef0a260c3edb9a21c79a33f9b1ef5092
Aleksey Fedotov <0xc0dec@gmail.com>
2017-04-13 12:45:57 +0500

    Fixed transform direction vectors calc

--- src/Demo1/Transform.h
+++ src/Demo1/Transform.h
@@ -119,73 +119,73 @@ inline auto Transform::getWorldPosition() const -> glm::vec3
 inline auto Transform::getWorldUp() const -> glm::vec3
 {
     auto m = getWorldMatrix();
-    return {m[0][1], m[1][1], m[2][1]};
+    return {m[1][0], m[1][1], m[1][2]};
 }
 
 inline auto Transform::getLocalUp() const -> glm::vec3
 {
     auto m = getMatrix();
-    return {m[0][1], m[1][1], m[2][1]};
+    return {m[1][0], m[1][1], m[1][2]};
 }
 
 inline auto Transform::getWorldDown() const -> glm::vec3
 {
     auto m = getWorldMatrix();
-    return {-m[0][1], -m[1][1], -m[2][1]};
+    return {-m[1][0], -m[1][1], -m[1][2]};
 }
 
 inline auto Transform::getLocalDown() const -> glm::vec3
 {
     auto m = getMatrix();
-    return {-m[0][1], -m[1][1], -m[2][1]};
+    return {-m[1][0], -m[1][1], -m[1][2]};
 }
 
 inline auto Transform::getWorldLeft() const -> glm::vec3
 {
     auto m = getWorldMatrix();
-    return {-m[0][0], -m[1][0], -m[2][0]};
+    return {-m[0][0], -m[0][1], -m[0][2]};
 }
 
 inline auto Transform::getLocalLeft() const -> glm::vec3
 {
     auto m = getMatrix();
-    return {-m[0][0], -m[1][0], -m[2][0]};
+    return {-m[0][0], -m[0][1], -m[0][2]};
 }
 
 inline auto Transform::getWorldRight() const -> glm::vec3
 {
     auto m = getWorldMatrix();
-    return {m[0][0], m[1][0], m[2][0]};
+    return {m[0][0], m[0][1], m[0][2]};
 }
 
 inline auto Transform::getLocalRight() const -> glm::vec3
 {
     auto m = getMatrix();
-    return {m[0][0], m[1][0], m[2][0]};
+    return {m[0][0], m[0][1], m[0][2]};
 }
 
 inline auto Transform::getWorldForward() const -> glm::vec3
 {
     auto m = getWorldMatrix();
-    return {m[0][2], m[1][2], m[2][2]};
+    return {-m[2][0], -m[2][1], -m[2][2]};
 }
 
 inline auto Transform::getLocalForward() const -> glm::vec3
 {
     auto m = getMatrix();
-    return {m[0][2], m[1][2], m[2][2]};
+    return {-m[2][0], -m[2][1], -m[2][2]};
 }
 
 inline auto Transform::getWorldBack() const -> glm::vec3
 {
     auto m = getWorldMatrix();
-    return {-m[0][2], -m[1][2], -m[2][2]};
+    return {m[2][0], m[2][1], m[2][2]};
 }
 
 inline auto Transform::getLocalBack() const -> glm::vec3
 {
     auto m = getMatrix();
-    return {-m[0][2], -m[1][2], -m[2][2]};
+    return {m[2][0], m[2][1], m[2][2]};
 }
 
 inline auto Transform::getChildrenCount() const -> uint32_t
