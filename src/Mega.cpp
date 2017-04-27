7bbeaae53baa6c0e9ce0dbbadf0a22895bcc134f
Aleksey Fedotov <0xc0dec@gmail.com>
2017-04-27 22:40:00 +0000

    Vulkan compilation smoke test

--- src/Mega.cpp
+++ src/Mega.cpp
@@ -3,7 +3,58 @@
     MIT license
 */
 
+#define VK_USE_PLATFORM_WIN32_KHR
+#include <vulkan.h>
+
+#include <SDL.h>
+#include <SDL_syswm.h>
+#include <windows.h>
+#include <vector>
+
+
 int main()
 {
+    SDL_Init(SDL_INIT_VIDEO | SDL_INIT_TIMER | SDL_INIT_EVENTS);
+
+    auto window = SDL_CreateWindow("Demo", SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 800, 600, SDL_WINDOW_ALLOW_HIGHDPI);
+
+    VkApplicationInfo appInfo {};
+    appInfo.sType = VK_STRUCTURE_TYPE_APPLICATION_INFO;
+    appInfo.pApplicationName = "";
+    appInfo.pEngineName = "";
+    appInfo.apiVersion = VK_API_VERSION_1_0;
+
+    std::vector<const char *> enabledExtensions {
+        VK_KHR_SURFACE_EXTENSION_NAME,
+        VK_KHR_WIN32_SURFACE_EXTENSION_NAME,
+        VK_EXT_DEBUG_REPORT_EXTENSION_NAME
+    };
+
+    std::vector<const char *> enabledLayers {
+        "VK_LAYER_LUNARG_standard_validation",
+    };
+
+    VkInstanceCreateInfo instanceInfo {};
+    instanceInfo.sType = VK_STRUCTURE_TYPE_INSTANCE_CREATE_INFO;
+    instanceInfo.pNext = nullptr;
+    instanceInfo.pApplicationInfo = &appInfo;
+
+    if (!enabledLayers.empty())
+    {
+        instanceInfo.enabledLayerCount = enabledLayers.size();
+        instanceInfo.ppEnabledLayerNames = enabledLayers.data();
+    }
+
+    if (!enabledExtensions.empty())
+    {
+        instanceInfo.enabledExtensionCount = static_cast<uint32_t>(enabledExtensions.size());
+        instanceInfo.ppEnabledExtensionNames = enabledExtensions.data();
+    }
+
+    VkInstance instance;
+    vkCreateInstance(&instanceInfo, nullptr, &instance);
+
+    SDL_Quit();
+
     return 0;
 }
