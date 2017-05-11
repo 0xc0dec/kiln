d1730eb70f901a114c6269f451e4db23ddb5dd5a
Aleksey Fedotov <0xc0dec@gmail.com>
2017-05-11 23:13:12 +0000

    Added stub for VulkanEffect

---
+++ src/VulkanEffect.cpp
@@ -0,0 +1,15 @@
+/*
+    Copyright (c) Aleksey Fedotov
+    MIT license
+*/
+
+#include "VulkanEffect.h"
+
+vk::Effect::Effect(const std::string &vsSrc, const std::string &fsSrc)
+{
+    // TODO
+}
+
+vk::Effect::~Effect()
+{
+}
---
+++ src/VulkanEffect.h
@@ -0,0 +1,35 @@
+/*
+    Copyright (c) Aleksey Fedotov
+    MIT license
+*/
+
+#pragma once
+
+#include "Vulkan.h"
+
+namespace vk
+{
+    class Effect final
+    {
+    public:
+        Effect(const std::string &vsSrc, const std::string &fsSrc);
+        ~Effect();
+
+        auto getVertexShader() const -> VkShaderModule;
+        auto getFragmentShader() const -> VkShaderModule;
+
+    private:
+        Resource<VkShaderModule> vertexShader;
+        Resource<VkShaderModule> fragmentShader;
+    };
+
+    inline auto Effect::getVertexShader() const -> VkShaderModule
+    {
+        return vertexShader;
+    }
+
+    inline auto Effect::getFragmentShader() const -> VkShaderModule
+    {
+        return fragmentShader;
+    }
+}
